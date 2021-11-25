a24a0d0b6aaae629a9207f06f0ddf59549bf1423
NanoFreelancePro <nano.freelance.pro@gmail.com>
2021-11-25 17:42:07 +0100

    debut js 2 de fromscratch

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* CALCULETTE */
 let total = 0;
 
 function plus(x) {
@@ -31,3 +32,31 @@ function fois(x) {
 function reset() {
   total = 0;
 }
+
+/* QUIZZZZZ */
+
+/* document.querySelector("h4").style.background = "yellow"; */
+
+/* const baliseHTML = document.querySelector("h4");
+console.log(baliseHTML);
+baliseHTML.style.background = "yellow"; */
+
+// Click event
+
+const questionContainer = document.querySelector(".click-event");
+const btn1 = document.getElementById("btn-1");
+const btn2 = document.getElementById("btn-2");
+const response1 = document.getElementById("response-1");
+
+questionContainer.addEventListener("click", () => {
+  questionContainer.classList.toggle("question-clicked");
+});
+
+btn1.addEventListener("click", () => {
+  response1.classList.toggle("show-true-response");
+  response1.innerText = "VRAI ! La réponse est Berne !";
+});
+btn2.addEventListener("click", () => {
+  response1.classList.toggle("show-false-response");
+  response1.innerText = "FAUX ! La réponse est BERNE !";
+});
